fc03ecb8167a31e44c8cb8a86974d9f399f0a1bb
Nestor Torre <160300116@ucaribe.edu,mx>
2019-05-12 02:13:53 -0500

    Update Trip.php

--- http-server/Project/Models/Trip.php
+++ http-server/Project/Models/Trip.php
@@ -126,8 +126,8 @@ class Trip extends DbModel
 				$trip_id = $this->id;
 				$role= 1;
 				$datetime1= $this->datetime;
-				$sql	= "SELECT * FROM `marker` INNER JOIN `trip` ON `trip`.`role`=:role AND `trip`.`datetime`=:datetime1";
-				$markerpasseger	= array_map([get_called_class(),"normalization"],Database::instance()->query($sql,array(':role' => $role, ':datetime1' => $datetime1)));
+				$sql	= "SELECT * FROM `marker` INNER JOIN `trip` ON `trip`.`role`=:role AND `marker`.`trip_id` =  `trip`.`id`";
+				$markerpasseger	= array_map([get_called_class(),"normalization"],Database::instance()->query($sql,array(':role' => $role)));
 
 				print_r ($markerpasseger);
 				if (empty($markerpasseger))

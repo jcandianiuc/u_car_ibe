8c87a16ffa5cd319501acdf0eac6dd355001cb5d
Nestor Torre <160300116@ucaribe.edu,mx>
2019-05-12 22:56:21 -0500

    Update Trip.php

--- http-server/Project/Models/Trip.php
+++ http-server/Project/Models/Trip.php
@@ -171,8 +171,18 @@ ENDOFQUERY;
 					foreach($markerpassenger as $marker){
 						if ($this->testMatch($marker, $routedriver ,200)) {
 								echo "yes";
+								echo $marker->trip_id;
+								echo $routedriver[0]->trip_id;
 								# verificar el trip id de pasajerp esta cancelado con otro trip id de conductor, o no este ese match
 								#Insertar el match encontrado 
+								#$matching = new Match(); # Creamos un nuevo match
+								# Guardamos los datos del nuevo usuario
+								#$matching->driver_trip_id = $idroute->trip_id;
+								#$matching->passenger_trip_id = $markerpassenger[0]->trip_id;
+								#$matching->driver_status = 0;
+								#$matching->passenger_status = 0;
+								# Insertar los datos en la BD
+								#$matching->save();
 								#return exito
 								return null;
 							}
@@ -236,7 +246,7 @@ ENDOFQUERY;
 								# Insertar los datos en la BD
 								$matching->save();
 								#return exito
-								echo "Se insertio un match";
+								#echo "Se insertio un match";
 								return null;
 							}
 
